a63d9d3d485817d8e41b2fbe71677be6437e7861
Kateryna Miach <kateryna.miach@nure.ua>
2025-06-12 00:04:39 +0300

    added api/auth

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3001/login", {
+      const response = await fetch("http://localhost:3001/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -79,7 +79,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3001/register", {
+      const response = await fetch("http://localhost:3001/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
